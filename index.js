ebed8788e7caa751d07f04fddcfcb53468998d91
Falco Nogatz <fnogatz@gmail.com>
2015-03-12 15:05:38 +0100

    Use JavaScript Standard Style

--- index.js
+++ index.js
@@ -1,183 +1,174 @@
-module.exports = createConsole;
-module.exports.load = load;
-module.exports.combine = combine;
-module.exports.getCellContent = getCellContent;
-module.exports.insert = insert;
-module.exports.insert.Array = insertArray;
-module.exports.insert.Object = insertObject;
+module.exports = createConsole
+module.exports.load = load
+module.exports.combine = combine
+module.exports.getCellContent = getCellContent
+module.exports.insert = insert
+module.exports.insert.Array = insertArray
+module.exports.insert.Object = insertObject
 
+var Table = require('cli-table')
+var requireAll = require('require-all')
 
-var Table = require('cli-table');
-var requireAll = require('require-all');
+function createConsole (objects) {
+  var oldLog = console.log
 
-
-function createConsole(objects) {
-  var oldLog = console.log;
-
-  var konsole = function printObject(input, fields) {
-    var str = toString(objects, input, fields);
+  var konsole = function printObject (input, fields) {
+    var str = toString(objects, input, fields)
     if (str) {
-      oldLog(str);
+      oldLog(str)
+    } else {
+      oldLog(input)
     }
-    else {
-      oldLog(input);
-    }
-  };
+  }
 
   // populate methods from console
   for (var key in console) {
-    konsole[key] = console[key];
+    konsole[key] = console[key]
   }
 
   // replace .log()
-  konsole.log = function log() {
-    var args = [];
-    var str;
+  konsole.log = function log () {
+    var args = []
+    var str
     for (var i = 0; i < arguments.length; i++) {
-      str = toString(objects, arguments[i]);
+      str = toString(objects, arguments[i])
       if (str) {
-        oldLog(str);
-      }
-      else {
-        args.push(arguments[i]);
+        oldLog(str)
+      } else {
+        args.push(arguments[i])
       }
     }
-    oldLog.apply(this, args);
-  };
+    oldLog.apply(this, args)
+  }
 
   // add .toString()
-  konsole.toString = function objectToString(input, fields) {
-    return toString(objects, input, fields);
-  };
+  konsole.toString = function objectToString (input, fields) {
+    return toString(objects, input, fields)
+  }
 
   // save config
-  konsole._tconsoleConfig = objects;
+  konsole._tconsoleConfig = objects
 
-  return konsole;
+  return konsole
 }
 
-
-function toString(objects, input, fields) {
-  for (var type in objects) {
+function toString (objects, input, fields) {
+  var type
+  for (type in objects) {
     if (objects[type].test && objects[type].test.call(input)) {
-      fields = fields || objects[type].defaultFields || Object.keys(objects[type].fields);
+      fields = fields || objects[type].defaultFields || Object.keys(objects[type].fields)
       if (fields === '*')
-        fields = Object.keys(objects[type].fields);
+        fields = Object.keys(objects[type].fields)
 
-      return fromObject(objects, input, type, fields);
+      return fromObject(objects, input, type, fields)
     }
   }
 
+  var satisfiesAll
+  var tester
   if (input instanceof Array) {
-    var satisfiesAll = false;
-    var tester;
+    satisfiesAll = false
 
-    for (var type in objects) {
-      tester = objects[type].test;
+    for (type in objects) {
+      tester = objects[type].test
       if (!tester && /^array:/.test(type) && objects[type.replace(/^array:/, '')]) {
-        tester = objects[type.replace(/^array:/, '')].test;
+        tester = objects[type.replace(/^array:/, '')].test
       }
 
-      satisfiesAll = input.every(function(row) {
-        return tester.call(row);
-      });
+      satisfiesAll = input.every(function (row) {
+        return tester.call(row)
+      })
 
       if (satisfiesAll) {
-        if (!fields && objects['array:'+type])
-          fields = objects['array:'+type].defaultFields || Object.keys(objects['array:'+type].fields);
+        if (!fields && objects['array:' + type])
+          fields = objects['array:' + type].defaultFields || Object.keys(objects['array:' + type].fields)
         if (!fields)
-          fields = objects[type].defaultFields || Object.keys(objects[type].fields);
+          fields = objects[type].defaultFields || Object.keys(objects[type].fields)
 
-        return fromObject(objects, input, type, fields, true);
+        return fromObject(objects, input, type, fields, true)
       }
     }
   }
 
   if (typeof input === 'object') {
-    var satisfiesAll = false;
-    var tester;
+    satisfiesAll = false
 
-    var keys = Object.keys(input).filter(function(key) {
-      return key[0] !== '_';
-    });
+    var keys = Object.keys(input).filter(function (key) {
+      return key[0] !== '_'
+    })
 
-    for (var type in objects) {
-      tester = objects[type].test;
+    for (type in objects) {
+      tester = objects[type].test
       if (!tester) {
-        if (/^array:/.test(type) && objects[type.replace(/^array:/, '')]) {      
-          tester = objects[type.replace(/^array:/, '')].test;
-        }
-        else {
-          throw new Error('No tester for type '+type);
+        if (/^array:/.test(type) && objects[type.replace(/^array:/, '')]) {
+          tester = objects[type.replace(/^array:/, '')].test
+        } else {
+          throw new Error('No tester for type ' + type)
         }
       }
 
-      satisfiesAll = keys.every(function(key) {
-        return tester.call(input[key]);
-      });
+      satisfiesAll = keys.every(function (key) {
+        return tester.call(input[key])
+      })
 
       if (satisfiesAll) {
-        if (!fields && objects['array:'+type])
-          fields = objects['array:'+type].defaultFields || Object.keys(objects['array:'+type].fields);
+        if (!fields && objects['array:' + type])
+          fields = objects['array:' + type].defaultFields || Object.keys(objects['array:' + type].fields)
         if (!fields)
-          fields = objects[type].defaultFields || Object.keys(objects[type].fields);
+          fields = objects[type].defaultFields || Object.keys(objects[type].fields)
 
-        var arr = keys.map(function(key) {
-          return input[key];
-        });
+        var arr = keys.map(function (key) {
+          return input[key]
+        })
 
-        return fromObject(objects, arr, type, fields);
+        return fromObject(objects, arr, type, fields)
       }
     }
   }
 
-  return false;
+  return false
 }
 
-
-function fromObject(objects, input, type, fields) {
-  var fieldNames = fields.map(function(field) {
+function fromObject (objects, input, type, fields) {
+  var fieldNames = fields.map(function (field) {
     if (typeof field === 'string')
-      return field;
+      return field
     if (typeof field === 'object' && field.hasOwnProperty('name'))
-      return field.name;
-    return '';
-  });
+      return field.name
+    return ''
+  })
 
+  var table
   if (input instanceof Array || objects[type].headers) {
-    var colAligns = [];
-    fields.forEach(function(field, ix) {
+    var colAligns = []
+    fields.forEach(function (field, ix) {
       if (typeof field === 'object' && field.align)
-        colAligns[ix] = field.align;
-    });
+        colAligns[ix] = field.align
+    })
 
-    var headers = objects[type].header ? objects[type].header(fieldNames, input) : fieldNames;
+    var headers = objects[type].header ? objects[type].header(fieldNames, input) : fieldNames
 
-    var table = new Table({
+    table = new Table({
       head: headers,
       colAligns: colAligns
-    });
-  }
-  else {
+    })
+  } else {
     // Vertical table
-    var table = new Table();
+    table = new Table()
   }
 
   if (objects[type].insert) {
-    objects[type].insert.call(input, table, objects[type], fieldNames);
-  }
-  else {
+    objects[type].insert.call(input, table, objects[type], fieldNames)
+  } else {
     if (!objects[type].fields && /^array:/.test(type)) {
-      insert.call(input, table, objects[type.replace(/^array:/, '')], fieldNames);
-    }
-    else {
-      insert.call(input, table, objects[type], fieldNames);
+      insert.call(input, table, objects[type.replace(/^array:/, '')], fieldNames)
+    } else {
+      insert.call(input, table, objects[type], fieldNames)
     }
   }
 
-  return table.toString();
+  return table.toString()
 }
-
 
 /**
  * Default renderer.insert function, `this` bound to the input.
@@ -185,17 +176,15 @@ function fromObject(objects, input, type, fields) {
  * @param  {Object} object     renderer object
  * @param  {Array} fields      fields to show
  */
-function insert(table, object, fields) {
-  var input = this;
+function insert (table, object, fields) {
+  var input = this
 
   if (input instanceof Array) {
-    insertArray.call(input, table, object, fields);
-  }
-  else {
-    insertObject.call(input, table, object, fields);
+    insertArray.call(input, table, object, fields)
+  } else {
+    insertObject.call(input, table, object, fields)
   }
 }
-
 
 /**
  * renderer.insert function for rendering arrays,
@@ -204,17 +193,16 @@ function insert(table, object, fields) {
  * @param  {Object} object     renderer object
  * @param  {Array} fields      fields to show
  */
-function insertArray(table, object, fields) {
-  var input = this;
+function insertArray (table, object, fields) {
+  var input = this
 
-  input.forEach(function addRow(entry, rowNo) {
-    var tableRow = fields.map(function cell(fieldName) {
-      return getCellContent.call(entry, object.fields[fieldName], rowNo);
-    });
-    table.push(tableRow);
-  });
+  input.forEach(function addRow (entry, rowNo) {
+    var tableRow = fields.map(function cell (fieldName) {
+      return getCellContent.call(entry, object.fields[fieldName], rowNo)
+    })
+    table.push(tableRow)
+  })
 }
-
 
 /**
  * renderer.insert function for rendering objects,
@@ -223,45 +211,43 @@ function insertArray(table, object, fields) {
  * @param  {Object} object     renderer object
  * @param  {Array} fields      fields to show
  */
-function insertObject(table, object, fields) {
-  var input = this;
+function insertObject (table, object, fields) {
+  var input = this
 
-  fields.forEach(function addField(field) {
-    var cells = {};
-    cells[field] = getCellContent.call(input, object.fields[field]);
-    table.push(cells);
-  });
+  fields.forEach(function addField (field) {
+    var cells = {}
+    cells[field] = getCellContent.call(input, object.fields[field])
+    table.push(cells)
+  })
 }
 
-
 /**
- * Get the content of a cell. `field` is the function or string 
+ * Get the content of a cell. `field` is the function or string
  *   to use. `this` should be bound to the actual entry.
  * Additional parameters are forwarded to the function `field`.
  * @param  {Function|String} field
  * @return {String}
  */
-function getCellContent(field) {
-  var entry = this;
-  var args = Array.prototype.slice.call(arguments, 1);
+function getCellContent (field) {
+  var entry = this
+  var args = Array.prototype.slice.call(arguments, 1)
 
   if (typeof field === 'string')
-    return field;
+    return field
   if (typeof field === 'function') {
-    var value;
+    var value
     try {
-      value = field.apply(entry, args);
+      value = field.apply(entry, args)
     } catch (e) {
-      value = '(err)';
+      value = '(err)'
     }
-    if (value === undefined || value === null) 
-      return '';
-    
-    return value.toString();
-  }
-  return '';
-}
+    if (value === undefined || value === null)
+      return ''
 
+    return value.toString()
+  }
+  return ''
+}
 
 /**
  * Create a console by requiring all renderers in a given
@@ -269,29 +255,27 @@ function getCellContent(field) {
  * @param  {String} location Filesystem path to use with require-all
  * @return {tconsole}
  */
-function load(location) {
-  var required = requireAll(location);
-  var config = {};
+function load (location) {
+  var required = requireAll(location)
+  var config = {}
   for (var key in required) {
-    config[key.replace(/^array\./, 'array:')] = required[key];
+    config[key.replace(/^array\./, 'array:')] = required[key]
   }
-  var konsole = createConsole(config);
-  return konsole;
+  var konsole = createConsole(config)
+  return konsole
 }
-
 
 /**
  * Combine multiple tconsole instances.
  * @return {tconsole}
  */
-function combine() {
-  var config = {};
-  Array.prototype.forEach.call(arguments, function(tconsoleInstance) {
-    mergeObject(config, tconsoleInstance._tconsoleConfig);
-  });
-  return createConsole(config);
+function combine () {
+  var config = {}
+  Array.prototype.forEach.call(arguments, function (tconsoleInstance) {
+    mergeObject(config, tconsoleInstance._tconsoleConfig)
+  })
+  return createConsole(config)
 }
-
 
 /**
  * Merge properties of obj2 into obj1.
@@ -299,6 +283,6 @@ function combine() {
  * @param  {Object} obj2
  * @return {Object}
  */
-function mergeObject(obj1, obj2){
-  for (var attrname in obj2) { obj1[attrname] = obj2[attrname]; }
+function mergeObject (obj1, obj2) {
+  for (var attrname in obj2) { obj1[attrname] = obj2[attrname] }
 }
--- test/index.js
+++ test/index.js
@@ -1,63 +1,61 @@
-var test = require('tap').test;
+var test = require('tap').test
 
-var tconsole = require('../index');
-var combine = tconsole.combine;
+var tconsole = require('../index')
+var combine = tconsole.combine
 
-
-function Person(first, last, age) {
-  this.first = first;
-  this.last = last;
-  this.age = age;
+function Person (first, last, age) {
+  this.first = first
+  this.last = last
+  this.age = age
 }
 
 var personFields = {
-  'First': function() { return this.first; },
-  'Last': function() { return this.last; },
-  'Name': function() { return this.first+' '+this.last; },
-  'Age': function() { return this.age; },
-};
+  'First': function () { return this.first },
+  'Last': function () { return this.last },
+  'Name': function () { return this.first + ' ' + this.last },
+  'Age': function () { return this.age }
+}
 
 var personConfig = {
   'person': {
-    test: function() {
-      return (this instanceof Person);
+    test: function () {
+      return (this instanceof Person)
     },
     fields: personFields,
     defaultFields: [
       'Name',
       'Age'
-    ],
+    ]
   },
   'array:person': {
     defaultFields: [
       'First',
       'Last',
       'Age'
-    ],
-  },
-};
+    ]
+  }
+}
 
 var dateConfig = {
   'date': {
-    test: function() {
-      return (this instanceof Date);
+    test: function () {
+      return (this instanceof Date)
     },
     fields: {
-      'Date': function() { return this.getDate(); },
-      'Year': function() { return this.getFullYear(); },
-      'Month': function() { return this.getMonth()+1; },
+      'Date': function () { return this.getDate() },
+      'Year': function () { return this.getFullYear() },
+      'Month': function () { return this.getMonth() + 1 }
     }
   }
-};
-
+}
 
 /**
  * Create object instances
  */
-var harry = new Person('Harry', 'Potter');
-var sirius = new Person('Sirius', 'Black', 37);
+var harry = new Person('Harry', 'Potter')
+var sirius = new Person('Sirius', 'Black', 37)
 // Sun Jan 18 2015 22:30:30 GMT+0100 (CET)
-var date = new Date(1421616630508);
+var date = new Date(1421616630508)
 
 /**
  * Expected outputs
@@ -65,65 +63,59 @@ var date = new Date(1421616630508);
 var strings = {
   Harry: '┌──────┬──────────────┐\n│ Name │ Harry Potter │\n├──────┼──────────────┤\n│ Age  │              │\n└──────┴──────────────┘',
   HarryAndSirius: '┌────────┬────────┬─────┐\n│ First  │ Last   │ Age │\n├────────┼────────┼─────┤\n│ Harry  │ Potter │     │\n├────────┼────────┼─────┤\n│ Sirius │ Black  │ 37  │\n└────────┴────────┴─────┘',
-  Date: '┌───────┬──────┐\n\│ Date  │ 18   │\n├───────┼──────┤\n│ Year  │ 2015 │\n├───────┼──────┤\n│ Month │ 1    │\n└───────┴──────┘',
-};
+  Date: '┌───────┬──────┐\n\│ Date  │ 18   │\n├───────┼──────┤\n│ Year  │ 2015 │\n├───────┼──────┤\n│ Month │ 1    │\n└───────┴──────┘'
+}
 
-
-test('has same footprint as console', function(t) {
-  var konsole = tconsole({});
+test('has same footprint as console', function (t) {
+  var konsole = tconsole({})
 
   for (var key in console) {
-    t.ok(konsole.hasOwnProperty(key), 'has .'+key+' property');
+    t.ok(konsole.hasOwnProperty(key), 'has .' + key + ' property')
   }
 
-  t.end();
-});
+  t.end()
+})
 
+test('.toString', function (t) {
+  var konsole = tconsole({})
 
-test('.toString', function(t) {
-  var konsole = tconsole({});
+  t.ok(konsole.hasOwnProperty('toString'))
+  t.type(konsole.toString, 'function')
 
-  t.ok(konsole.hasOwnProperty('toString'));
-  t.type(konsole.toString, 'function');
+  t.end()
+})
 
-  t.end();
-});
+test('Example: Person', function (t) {
+  var konsole = tconsole(personConfig)
 
+  t.equal(konsole.toString(harry), strings.Harry)
+  t.end()
+})
 
-test('Example: Person', function(t) {
-  var konsole = tconsole(personConfig);
+test('Example: Array of Person', function (t) {
+  var konsole = tconsole(personConfig)
 
-  t.equal(konsole.toString(harry), strings.Harry);
-  t.end();
-});
+  t.equal(konsole.toString([ harry, sirius ]), strings.HarryAndSirius)
 
+  t.end()
+})
 
-test('Example: Array of Person', function(t) {
-  var konsole = tconsole(personConfig);
+test('Example: Date', function (t) {
+  var konsole = tconsole(dateConfig)
 
-  t.equal(konsole.toString([ harry, sirius ]), strings.HarryAndSirius);
+  t.equal(konsole.toString(date), strings.Date)
 
-  t.end();
-});
+  t.end()
+})
 
+test('combine', function (t) {
+  var konsole1 = tconsole(personConfig)
+  var konsole2 = tconsole(dateConfig)
 
-test('Example: Date', function(t) {
-  var konsole = tconsole(dateConfig);
+  var konsole = combine(konsole1, konsole2)
 
-  t.equal(konsole.toString(date), strings.Date);
+  t.equal(konsole.toString(date), strings.Date)
+  t.equal(konsole.toString(harry), strings.Harry)
 
-  t.end();
-});
-
-
-test('combine', function(t) {
-  var konsole1 = tconsole(personConfig);
-  var konsole2 = tconsole(dateConfig);
-
-  var konsole = combine(konsole1, konsole2);
-
-  t.equal(konsole.toString(date), strings.Date);
-  t.equal(konsole.toString(harry), strings.Harry);
-
-  t.end();
-});
+  t.end()
+})
